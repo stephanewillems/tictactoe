845b077b420898d98add21198d8211ffd8982e1b
StephaneWillems <stephanewillems@gmail.com>
2020-07-04 22:22:35 +0200

    added data-index atribute to html and created 2 module: 1° Domcontrol for the control of the dom and creating of player +render names. 2° creation of gamelogic: players click on field in board, value of data-index is returned

--- script.js
+++ script.js
@@ -21,8 +21,138 @@
 
 // PLAYER FACTORY FUNCTION we hebben er meer als 1 nodig dus maak je een factory
 
-const PlayerFactory = (name) => {
-    const sayHello = () => console.log(`Hello ${name}`);
-    return  { name, sayHello };
-};
+const Player =(name, turn)=>{
+    name;
+    turn;
 
+  return{name,turn};
+}
+
+//Domcontrol : All buttons and controls
+const Domcontrol = (function(){
+    //local scope
+
+    let playerOne;
+    let playerTwo;
+    let start = false;
+
+    //cache dom
+    const startBtn = document.querySelector(".start");
+    const resetBtn = document.querySelector(".reset");
+    const aiBtn = document.querySelector(".computer");
+    const playerOneTag = document.querySelector(".player1");
+    const playerTwoTag = document.querySelector(".player2");
+    const counterP1 =  document.querySelector('.counterP1');
+    const counterP2 =  document.querySelector('.counterP2');
+    const gridEl = document.querySelectorAll('.board');
+    
+
+    //bind events
+    startBtn.addEventListener("click",_createPlayer);
+    
+    resetBtn.addEventListener("click", _resetGame);
+        //bind x
+        //bind O
+        // bind AI
+
+
+
+    //create a player PRIVATE FUNCTION
+    function _createPlayer(){
+        playerOne = Player(prompt("what is your name ,player 1?"), true);
+        playerTwo = Player(prompt("what is your name, player 2?"), false);
+        start = true;
+        _render();
+        return  playerOne, playerTwo ;
+        
+    }
+
+    // TEST PUBLIC FUNCTION
+    function logconsole(){
+        console.log( playerOne.name + " versus " + playerTwo.name );
+        return {playerOne, playerTwo};
+    }
+
+    //render page with names
+    function _render(){
+        if(Object.getOwnPropertyNames(playerOne).length !== 0){
+            playerOneTag.innerHTML = `<h2> ${playerOne.name}</h2>`;
+            playerTwoTag.innerHTML = `<h2> ${playerTwo.name}</h2>`;
+        }else{
+            playerOneTag.innerHTML = `<h2>Player One</h2>`;
+            playerTwoTag.innerHTML = `<h2>Player Two</h2>`;
+        }
+       
+    }
+
+    function _resetGame(){
+        start = false;
+        playerOne = {};
+        playerTwo = {};
+        console.log("reset!");
+        _render();
+        
+    }
+    //GETTERS
+    function getStart(){
+        return start;
+    }
+
+    function getPlayerOne(){
+            return playerOne;
+    }
+    function getPlayerTwo(){
+        return playerTwo;
+}
+
+    return{
+        logconsole,
+        getPlayerOne,
+        getPlayerTwo,
+        playerTwo,
+        getStart,
+        gridEl
+    };
+})()
+
+
+//GAMELOGIC MODULE
+
+const Gamelogic = (function(){
+
+    let boardFields = [];
+    //get value from PlayerOne;
+    let valOne = () => Domcontrol.getPlayerOne();
+    let valTwo = () => Domcontrol.getPlayerTwo();
+    Domcontrol.gridEl.forEach((item)=>{item.addEventListener("click",_gridPlay);});
+
+    //CLICK on grid WHEN started PRIVATE function
+    function _gridPlay(e){
+        if(Domcontrol.getStart()){
+
+                
+           console.log(`${valOne().name} clicked on ${e.target.dataset.board}`);
+
+
+        }else{
+            console.log("not started yet");
+        }      
+
+}
+
+    function getvalOne(){
+        return valOne,valTwo;
+    }
+   function useStart(){
+      console.log(Domcontrol.getStart());
+   }
+
+    return {
+        useStart : useStart,
+        valOne,
+        valTwo,
+        getvalOne
+
+    }
+
+})()
